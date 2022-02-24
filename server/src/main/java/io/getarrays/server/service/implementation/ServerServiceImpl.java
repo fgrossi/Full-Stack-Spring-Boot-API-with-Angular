4293829b624c25f3aa5c170cce0f380d9b2db20b
Francesco Grossi <francesco.grossi@accenture.com>
2022-02-24 12:59:09 +0100

    service added

--- server/src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
+++ server/src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
@@ -1,14 +1,25 @@
 package io.getarrays.server.service.implementation;
 
+import io.getarrays.server.enumeration.Status;
 import io.getarrays.server.model.Server;
 import io.getarrays.server.repo.ServerRepo;
 import io.getarrays.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
+
+import static io.getarrays.server.enumeration.Status.SERVER_UP;
+import static io.getarrays.server.enumeration.Status.SERVER_DOWN;
+import static java.lang.Boolean.TRUE;
+import static org.springframework.data.domain.PageRequest.of;
 
 @RequiredArgsConstructor
 @Service
@@ -25,33 +36,45 @@ public class ServerServiceImpl implements ServerService {
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        log.info("Saving new server {}", server.getName());
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000)  ? SERVER_UP : SERVER_DOWN);
+        return serverRepo.save(server);
     }
 
     @Override
-    public Collection<Server> list(int limit) {
-        return null;
+    public Collection<Server> list(int limit)
+    {
+        log.info("Fetching all servers");
+        return serverRepo.findAll(of(0,limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by Id: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by ID {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
+
     }
 
 
     private String setServerImageUrl() {
+        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+        ServletUriComponentsBuilder.fromCurrentContextPath().path("server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
         return null;
     }
 }
